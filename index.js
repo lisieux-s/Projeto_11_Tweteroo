b2abf9c40893ce836b0b11baf8a3db3e291abd48
Sophia de Lisieux Zocoler <sophiazocoler@gmail.com>
2022-01-23 20:36:14 -0300

    feat: sign-up, send tweet

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 import express from "express";
 const server = express();
+server.use(express.json())
+
+const users = [];
+const tweets = [];
+
+server.post('/sign-up', (req, res) => {
+    let user = req.body;
+    users.push(user);
+    res.send('OK');
+}) 
+
+server.post('/tweets', (req, res) => {
+    let tweet = req.body;
+    tweets.push(tweet);
+    res.send('OK');
+})
+
+server.get('/tweets', (req, res) => {
+    let latestTweets = [];
+
+    for(let i = tweets.length-1; i > tweets.length - 10; i--) {
+        latestTweets.push(tweets[i])
+    }
+    res.send(latestTweets);
+})
 
 server.listen(5000)
+console.log('running on port 5000')
